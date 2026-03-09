c63655842e89126512b750438e42834799f12abb
BLACKOUT-T <ralphaelfeliciano02@gmail.com>
2026-03-10 01:29:47 +0800

    FILES FOR ZONE LEADER WT EDITOR

---
+++ zone-leader/zone leader.js
@@ -0,0 +1,7 @@
+var map = L.map('map');
+map.setView([12.8797, 121.7740], 6);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
